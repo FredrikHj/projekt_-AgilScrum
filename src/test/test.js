4362c8b1b5bbbc54b129f60953d6aea7998ec471
GitRall <rasmus_olsson_89@hotmail.com>
2020-01-02 14:34:40 +0100

    deconstructured function import in test

--- src/test/test.js
+++ src/test/test.js
@@ -2,37 +2,39 @@
 /* eslint-disable prefer-arrow-callback, func-names */
 
 const assert = require('assert');
-const toDest = require('../utils/index');
+const { toDest } = require('../utils/index');
 
 describe('Tests', function () {
   describe('toDest function', function () {
-    let data = [
-      {
-        color: 'w', from: 'a2', to: 'a3', flags: 'n', piece: 'p', san: 'a3',
-      },
-      {
-        color: 'w', from: 'a2', to: 'a4', flags: 'b', piece: 'p', san: 'a4',
-      },
-      {
-        color: 'w', from: 'b2', to: 'b3', flags: 'n', piece: 'p', san: 'b3',
-      },
-      {
-        color: 'w', from: 'b2', to: 'b4', flags: 'b', piece: 'p', san: 'b4',
-      },
-      {
-        color: 'w', from: 'c2', to: 'c3', flags: 'n', piece: 'p', san: 'c3',
-      },
-      {
-        color: 'w', from: 'c2', to: 'c4', flags: 'b', piece: 'p', san: 'c4',
-      },
-    ];
-    let expected = {
-      a2: ['a3', 'a4'],
-      b2: ['b3', 'b4'],
-      c2: ['c3', 'c4'],
-    };
+    let data;
+    let expected;
     it('should return correct formatted data', function () {
-      assert.deepStrictEqual(toDest.toDest(data), expected);
+      data = [
+        {
+          color: 'w', from: 'a2', to: 'a3', flags: 'n', piece: 'p', san: 'a3',
+        },
+        {
+          color: 'w', from: 'a2', to: 'a4', flags: 'b', piece: 'p', san: 'a4',
+        },
+        {
+          color: 'w', from: 'b2', to: 'b3', flags: 'n', piece: 'p', san: 'b3',
+        },
+        {
+          color: 'w', from: 'b2', to: 'b4', flags: 'b', piece: 'p', san: 'b4',
+        },
+        {
+          color: 'w', from: 'c2', to: 'c3', flags: 'n', piece: 'p', san: 'c3',
+        },
+        {
+          color: 'w', from: 'c2', to: 'c4', flags: 'b', piece: 'p', san: 'c4',
+        },
+      ];
+      expected = {
+        a2: ['a3', 'a4'],
+        b2: ['b3', 'b4'],
+        c2: ['c3', 'c4'],
+      };
+      assert.deepStrictEqual(toDest(data), expected);
     });
     it('should return correct format when only from and to is sent', function () {
       data = [
@@ -48,7 +50,7 @@ describe('Tests', function () {
         b2: ['b3', 'b4'],
         c2: ['c3', 'c4'],
       };
-      assert.deepStrictEqual(toDest.toDest(data), expected);
+      assert.deepStrictEqual(toDest(data), expected);
     });
   });
 });
