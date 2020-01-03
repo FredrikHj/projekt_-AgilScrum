c63a4da249a600ffbafb2bd5eb9dbb767bb5cc04
GitRall <rasmus_olsson_89@hotmail.com>
2020-01-03 15:22:14 +0100

    added modal component to project

--- src/components/pages/gamePage.js
+++ src/components/pages/gamePage.js
@@ -1,10 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ChessBoard from '../ChessBoard/ChessBoard';
+import Modal from '../Modal/Modal';
+
 
 function GamePage(props) {
   const [data, setData] = useState({});
+  const [showWinnerModal, setShowWinnerModal] = useState(false);
+  const [isWinner] = useState(true);
   // const [username, setUsername] = useState('');
+
+  function gameResultContent() {
+    if (isWinner) {
+      return (
+        <>
+          <p className="modal-text">Congratulations you won!</p>
+          <button type="button">End Game</button>
+        </>
+
+      );
+    }
+    return (
+      <>
+        <p>Better luck next time!</p>
+        <button type="button">End Game</button>
+      </>
+    );
+  }
+
+  function closeWinnerModal() {
+    setShowWinnerModal(false);
+  }
 
   function getColor() {
     /* for (const key in data.players) {
@@ -34,11 +60,15 @@ function GamePage(props) {
   }, []);
 
   return (
-    <div className="board-container">
-      { Object.keys(data).length
-        ? <ChessBoard color={getColor()} fenKey={data.fen} postMove={postMove} />
-        : null}
-    </div>
+    <>
+      <Modal title={isWinner ? 'Winner' : 'Loser'} content={gameResultContent()} show={showWinnerModal} close={closeWinnerModal} />
+      <div className="board-container">
+        { Object.keys(data).length
+          ? <ChessBoard color={getColor()} fenKey={data.fen} postMove={postMove} />
+          : null}
+      </div>
+    </>
+
   );
 }
 
