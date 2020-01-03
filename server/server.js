4cd753968770ae99e9a97eace0ae409214d8869b
osynkad <36000176+osynkad@users.noreply.github.com>
2020-01-03 16:24:22 +0100

    fixed cors middlware

--- server/server.js
+++ server/server.js
@@ -23,7 +23,9 @@ app.listen(PORT, () => {
 });
 
 app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Orgin', '*');
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
