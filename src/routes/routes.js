8dec936747f90a3801e1ed06abe0826fed47b373
Your Name <you@example.com>
2020-01-05 16:53:15 +0100

    terst3ewf

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,7 +4,7 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import { StartPage, LobbyPage, GamePage } from '../components/pages';
+import { StartPage, LobbyPage, GamePage } from '../omponents/pages';
 
 const routes = (
   <Router>
